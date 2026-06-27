1d25c49160dfdae868fa0516ca5c72c98283c8a9
LuisTriviño <luis.trivino@ucp.edu.co>
2026-06-27 12:55:55 -0500

    reset repo para nuevo proyecto PWA

--- src/design-system/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { Button, MotionButton } from './Button';
-export { Badge } from './Badge';
-export { Input } from './Input';
-export { Switch } from './Switch';
-export { Skeleton } from './Skeleton';
--- src/utils/date.ts
+++
@@ -1,6 +0,0 @@
-export function format(iso: string) {
-  return new Intl.DateTimeFormat('es', {
-    dateStyle: 'medium',
-    timeStyle: 'short',
-  }).format(new Date(iso));
-}
